c70ca8fe5955727a6de260eb625451c0fb36f4e3
gyjlovelh <18062791691@163.com>
2018-10-18 16:05:12 +0800

    auto commit

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-cd ../
+cd ~/Documents/angular6/netease-theme
 # yarn安装
 
 npm version patch
 if [ $? == 0 ]
 then echo "升级theme工程版本成功!"
 else
-echo "升级theme工程版本失败!"
-exit
+git commit -a -m "auto commit"
+echo "auto commit"
 fi
 
 npm run package
 if [ $? == 0 ]
 then echo "打包theme成功!"
 else
-echo "打包theme失败!"
+echo "打包theme失败"
 exit
 fi
 
